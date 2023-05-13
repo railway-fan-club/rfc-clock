76825856f0d9a26212f50011e050a22d63d72360
yuuki-yano <yuuki.yano.1103@gmail.com>
2023-05-13 22:13:02 +0900

    fix PWAついでにJSファイルなどを分離
    manifest.jsonが読み込めない問題を解決しました。これでPWA化
    DIVの位置が違ったので変更
    JSファイル、CSSファイルを外部ファイルへ分離

--- sw.js
+++ sw.js
@@ -4,24 +4,31 @@ const CACHE_NAME = 'rfc-clock-caches-v1';
 const urlsToCache = [
     './',
     './index.html',
+    './css/style.css',
+    './js/clock.js',
+    './js/hanabi.js',
+    './lib/p5.min.js',
+    './img/logo.png',
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches
-        .open(CACHE_NAME)
-        .then((cache) => {
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log('Opened cache');
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches
-        .match(event.request)
-        .then((response) => {
-            return response ? response : fetch(event.request);
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
         })
     );
 });
